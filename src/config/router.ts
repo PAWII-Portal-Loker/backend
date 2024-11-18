d0888bee427b5405ebcb14fcbec92919ed8300ef
Fanesz <fanes23.pratama@mhs.mdp.ac.id>
2024-11-18 13:32:43 +0700

    add: snake cased json key middleware

--- src/config/router.ts
+++ src/config/router.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import { toSnakeCase } from "@utils/caseConvert";
+import express, { NextFunction, Request, Response } from "express";
 
 class Router {
   app: express.Application;
@@ -6,7 +7,9 @@ class Router {
 
   constructor(port: string | number) {
     this.app = express();
+    this.app.use(this.snakeCaseHandler());
     this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
     this.port = Number(port);
   }
 
@@ -14,6 +17,19 @@ class Router {
     this.app.listen(this.port, () => {
       console.log(`Server is running on port ${this.port}`);
     });
+  }
+
+  snakeCaseHandler() {
+    return (_: Request, res: Response, next: NextFunction) => {
+      const originalSend = res.send.bind(res);
+
+      res.send = (body: unknown): Response => {
+        const snakeCasedBody = toSnakeCase(JSON.parse(body as string));
+        return originalSend(JSON.stringify(snakeCasedBody));
+      };
+
+      next();
+    };
   }
 }
 
