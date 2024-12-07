fdf6c6a2c16864b2b3303e233715f21d8ee883f3
Fanesz <fanes23.pratama@mhs.mdp.ac.id>
2024-12-07 21:15:21 +0700

    edit: consts into normal string form

--- src/modules/enums/consts/jobTypes.ts
+++ src/modules/enums/consts/jobTypes.ts
@@ -1,13 +1,15 @@
 export type JobTypeEnums = string;
 
-export const JOB_TYPE_FULL_TIME: JobTypeEnums = "FULL_TIME";
-export const JOB_TYPE_PART_TIME: JobTypeEnums = "PART_TIME";
-export const JOB_TYPE_INTERNSHIP: JobTypeEnums = "INTERNSHIP";
-export const JOB_TYPE_CONTRACT: JobTypeEnums = "CONTRACT";
+export const JOB_TYPE_INTERNSHIP: JobTypeEnums = "Internship";
+export const JOB_TYPE_PART_TIME: JobTypeEnums = "Part Time";
+export const JOB_TYPE_FULL_TIME: JobTypeEnums = "Full Time";
+export const JOB_TYPE_CONTRACT: JobTypeEnums = "Contract";
+export const JOB_TYPE_OTHER: JobTypeEnums = "Other";
 
 export const JOB_TYPES = [
-  JOB_TYPE_FULL_TIME,
-  JOB_TYPE_PART_TIME,
   JOB_TYPE_INTERNSHIP,
+  JOB_TYPE_PART_TIME,
+  JOB_TYPE_FULL_TIME,
   JOB_TYPE_CONTRACT,
+  JOB_TYPE_OTHER,
 ];
