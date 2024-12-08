cd1815f0ebfa98c44c923520f2dff6ab1a254199
Fanesz <fanes23.pratama@mhs.mdp.ac.id>
2024-12-08 12:25:26 +0700

    delete: templete

--- src/modules/z_templete/models/xxxxx.model.ts
+++
@@ -1,9 +0,0 @@
-// import mongoose, { Schema } from "mongoose";
-
-// const xxxxSchema = new Schema<XxxxDto>({
-//   xx: { type: mongoose.Schema.Types.ObjectId, required: true },
-//   xxx: { type: String, required: true, unique: true },
-//   xxxx: { type: String, required: true },
-// });
-
-// export const UserModel = mongoose.model<XxxxDto>("Xxxx", xxxxSchema);
